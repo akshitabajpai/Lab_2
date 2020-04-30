f8a58221e8334bbe3291e025e227f690d6551d40
DELL <DELL@DESKTOP-FRIMMDM>
2020-04-30 15:56:23 +0530

    final commit

---
+++ Lab2/src/main/java/akshita/cg/lab2/trainee/dao/TraineeDao.java
@@ -0,0 +1,15 @@
+package akshita.cg.lab2.trainee.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import akshita.cg.lab2.trainee.entity.Trainee;
+
+@Transactional
+@Repository
+public interface TraineeDao extends JpaRepository<Trainee, Integer> {
+
+
+}
